de719bcda572fae27883c8bf881091b26aa5ded2
Jakub Zárybnický <kuba.zarybnicky@post.cz>
2014-09-19 12:57:00 +0200

    Code Golf v.1

--- program/program.ino
+++ program/program.ino
@@ -1,61 +1,19 @@
 #include <Servo.h> 
-
 Servo mLeft, mRight;
-
-int speed = 90, on = 0, turning = 0, direction = 0;
-
-void setup() 
-{
-  pinMode(7, INPUT);
-  pinMode(9, INPUT);
-  pinMode(10, INPUT);
-  pinMode(11, INPUT);
+int speed = 90;
+void setup() {
   mLeft.attach(12);
   mRight.attach(13);
-  Serial.begin(9600); 
-  //while (!Serial) {}
-} 
-
-void printDebug(){
-  Serial.print(digitalRead(11));
-  Serial.print(" ");
-  Serial.print(digitalRead(9));
-  Serial.print(" ");
-  Serial.print(digitalRead(10));
-  Serial.println();
 }
-
 void loop() {
-  /*
-  if (digitalRead(7)) {
-    on = !on;
-    delay(1000);
-  }
-  if(!on) {
-    return;
-  }
-  */
-
-  evade(digitalRead(11), digitalRead(9), digitalRead(10));
-}
-
-void evade(int left, int center, int right) {
-  if (center) {
+  if (digitalRead(9)) {
     go(speed, speed);
-    direction = 0;
-    return; 
-  }
-  
-  if (left || direction == 1) {
+  } else if (digitalRead(11)) {
     go(speed, -speed / 2);
-    direction = 1;
-  }
-  if (right || direction == -1) {
+  } else if (digitalRead(10)) {
     go(-speed / 2, speed);
-    direction = -1;
   }
 }
-
 void go(int lSpeed, int rSpeed) {
   mLeft.write(90 - lSpeed);
   mRight.write(90 + rSpeed);
